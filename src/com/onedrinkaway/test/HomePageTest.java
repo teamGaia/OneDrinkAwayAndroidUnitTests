30e64da2168e1474036c2f23381e78949235ede8
lueyja1 <lueyja1@gmail.com>
2014-05-23 13:59:15 -0700

    Update HomePageTest.java
    added an actual test case

--- src/com/onedrinkaway/test/HomePageTest.java
+++ src/com/onedrinkaway/test/HomePageTest.java
@@ -1,12 +1,15 @@
 package com.onedrinkaway.test;
 
+import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
 import com.onedrinkaway.app.*;
 import com.onedrinkaway.R;
 
 public class HomePageTest extends ActivityInstrumentationTestCase2<HomePage> {
 	private HomePage activity;
-	//private View myView;
+	private View myView;
 	
 	public HomePageTest(){
 		super(HomePage.class);
@@ -16,7 +19,17 @@ public class HomePageTest extends ActivityInstrumentationTestCase2<HomePage> {
 	protected void setUp() throws Exception {
 		super.setUp();
 		activity = getActivity();
+		myView = activity.findViewById(R.layout.activity_home_page);
 		
-		//myView = activity.findViewById(R.id.home);
+
+	}
+	
+	public void testSimple(){
+		ActivityMonitor monitor = getInstrumentation().addMonitor(SearchByName.class.getName(), null, false);
+		assertTrue(activity.getSupportActionBar().getTitle().equals(R.string.app_name));
+		activity.goToActivity(activity.findViewById(R.id.search_by_name));
+		getInstrumentation().waitForIdleSync();
+		SearchByName nextPage = (SearchByName) getInstrumentation().waitForMonitor(monitor);
+		assertNotNull(nextPage);
 	}
 }
